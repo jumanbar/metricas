d615854a5baf0ef2aa75fa512e30bf056a32b506
Juan M. Barreneche <jumanbar@gmail.com>
2023-08-27 00:09:02 -0300

    Indentacion

--- server.R
+++ server.R
@@ -1,47 +1,41 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
+srvfun <- function(input, output) {
+
+	d <- reactive({
+		file <- input$archivo
+		ext <- tools::file_ext(file$datapath)
+		req(file)
+		validate(need(ext %in% c("csv", "txt"),
+			      "Por favor, subir un archivo con extensión .csv o .txt"))
+		out <- read.csv(file$datapath, header = TRUE, stringsAsFactors = FALSE)
+		names(out) <- tolower(names(out))
+		return(out)
+	})
+
+	output$scatter <- renderPlot({
+		g_em(d(), alpha = .7)
+	})
+
+	output$metricas_out <- DT::renderDT({
+		out <- calc_metricas(d())
+		out$valor <- round(out$valor, 2)
+		DT::datatable(
+			      extensions = 'Buttons',
+			      class = 'cell-border stripe',
+			      rownames = FALSE,
+			      options = list(
+					     dom = "Bf",
+					     pageLength = nrow(out),
+					     paging = FALSE,
+					     buttons = list("copy", "csv", "excel")
+					     ),
+			      data = out
+			      )
+	})
+
+}
+
 # Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+shinyServer(srvfun)
 
-  d <- reactive({
-    file <- input$archivo
-    ext <- tools::file_ext(file$datapath)
-    req(file)
-    validate(need(ext %in% c("csv", "txt"),
-                  "Por favor, subir un archivo con extensión .csv o .txt"))
-    out <- read.csv(file$datapath, header = TRUE, stringsAsFactors = FALSE)
-    names(out) <- tolower(names(out))
-    return(out)
-  })
-
-  output$scatter <- renderPlot({
-        g_em(d(), alpha = .7)
-    })
-
-  output$metricas_out <- DT::renderDT({
-    out <- calc_metricas(d())
-    out$valor <- round(out$valor, 2)
-    DT::datatable(
-      extensions = 'Buttons',
-      class = 'cell-border stripe',
-      rownames = FALSE,
-      options = list(
-        dom = "Bf",
-        pageLength = nrow(out),
-        paging = FALSE,
-        buttons = list("copy", "csv", "excel")
-      ),
-      data = out
-    )
-  })
-
-})
--- ui.R
+++ ui.R
@@ -1,44 +1,38 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  # Application title
-  titlePanel("Cálculo de métricas"),
+ui <- fluidPage(
+		# Application title
+		titlePanel("Cálculo de métricas"),
 
-  # Sidebar with a slider input for number of bins
-  sidebarLayout(
-    sidebarPanel(
-      fileInput(
-        'archivo',
-        'Cargar archivo CSV',
-        buttonLabel = 'Buscar...',
-        placeholder = 'Sin archivo seleccionado aún'
-      ),
-      helpText(
-        'El archivo debe ser texto separado por comas, con puntos',
-        'para decimales; además debe tener una columna "m" con',
-        'valores medidos in situ, y otra columna "e", con valores',
-        'estimados por el modelo (siempre hablando de Clorofila-a)'
-      ),
-      tags$h4("Enlaces:"),
-      tags$a("Archivo CSV de ejemplo", href = 'ejemplo.csv'),
-      tags$br(),
-      tags$a("Documentación de las métricas", href = 'metricas.pdf')
-    ),
+		# Sidebar with a slider input for number of bins
+		sidebarLayout(
+			      sidebarPanel(
+					   fileInput(
+						     'archivo',
+						     'Cargar archivo CSV',
+						     buttonLabel = 'Buscar...',
+						     placeholder = 'Sin archivo seleccionado aún'
+						     ),
+					   helpText(
+						    'El archivo debe ser texto separado por comas, con puntos',
+						    'para decimales; además debe tener una columna "m" con',
+						    'valores medidos in situ, y otra columna "e", con valores',
+						    'estimados por el modelo (siempre hablando de Clorofila-a)'
+						    ),
+					   tags$h4("Enlaces:"),
+					   tags$a("Archivo CSV de ejemplo", href = 'ejemplo.csv'),
+					   tags$br(),
+					   tags$a("Documentación de las métricas", href = 'metricas.pdf')
+					   ),
 
-    mainPanel(
-      plotOutput("scatter"),
-      DT::DTOutput('metricas_out')
-      ),
+			      mainPanel(
+					plotOutput("scatter"),
+					DT::DTOutput('metricas_out')
+					),
 
-  )
-))
+			      )
+		)
+
+shinyUI(ui)
+
